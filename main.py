926168396ffcd54dc56a50ba720b97c900e06d76
0xInfection <theinfecteddrake@gmail.com>
2020-08-21 22:17:39 +0530

    Added main file

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#-:-:-:-:-:-:-::-:-:#
+#     SIP Torch     #
+#-:-:-:-:-:-:-::-:-:#
+
+# Author: 0xInfection
+# This module requires SIPTorch
+# https://github.com/0xInfection/SIPTorch
+
+# Importing the tests
+from tests.parser import ()
+from tests.invalid import ()
+from tests.backcomp import ()
+from tests.application import ()
+from tests.transaction import ()
+
+# Requestor modules
+from requester import (
+    buildreq, 
+    connector,
+    parser
+)
+
