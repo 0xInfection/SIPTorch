cc8b91ba32617ec66c8959a2c110eb44fcdbe246
0xInfection <theinfecteddrake@gmail.com>
2020-08-24 23:01:11 +0530

    add wsinv

---
+++ tests/parser/wsinv.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#-:-:-:-:-:-:-::-:-:#
+#     SIP Torch     #
+#-:-:-:-:-:-:-::-:-:#
+
+# Author: 0xInfection
+# This module requires SIPTorch
+# https://github.com/0xInfection/SIPTorch
+
+# Essentially this is what I am aiming to achieve
+
+def wsinv():
+    '''
+    Return a malformed yet valid SIP message
+    '''
+    module_info = {
+        'category'  :   'Parser Tests',
+        'type'      :   'Valid Messages',
+        'test'      :   'Short Torturous INVITE',
+        'id'        :   'wsinv',
+        'coverage'  :   ''
+    }
