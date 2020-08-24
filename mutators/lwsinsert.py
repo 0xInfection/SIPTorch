30cedaef2c34532ea6037a79a1b31812ba398d95
0xInfection <theinfecteddrake@gmail.com>
2020-08-24 22:59:17 +0530

    add crlf

--- mutators/lwsinsert.py
+++ mutators/lwsinsert.py
@@ -23,7 +23,7 @@ UP_CONST = 7
 # Lower constraint
 LW_CONST = 3
 
-def lwsInsert(msg: str, charc='random'):
+def lwsInsert(msg: str, crlf=False, charc='random'):
     '''
     Inserts random characters into strings randomly.
     By default it inserts whitespaces, but you can use a string too.
@@ -34,11 +34,16 @@ def lwsInsert(msg: str, charc='random'):
         return
     wschars = [TAB, EN_QUAD, EM_QUAD, WS, IDG_SEP]
     # Generate the character sequence
-    if charc is 'random':
+    if charc == 'random':
         # Fetching all whitespace chars
         log.debug('Performing random LWS mutation')
-        return ''.join('%s%s' % (x, random.choice((random.choice(wschars), ""))) for x in msg)
+        s = ''.join('%s%s' % (x, random.choice((random.choice(wschars), ""))) for x in msg)
     else:
         log.debug('Performing single LWS mutation')
-        return ''.join('%s%s' % (x, random.choice((charc, ""))) for x in msg)
-    
+        s = ''.join('%s%s' % (x, random.choice((charc, ""))) for x in msg)
+    if crlf:
+        log.debug('Applying CRLF transformation')
+        for x in s:
+            if x in wschars:
+                s = s.replace(x, x+'\r\n'+x) if random.random() > 0.9 else s
+    return s
