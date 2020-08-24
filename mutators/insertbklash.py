06962c15805a329a16ccde98c5b8ed5c122fe7fc
0xInfection <theinfecteddrake@gmail.com>
2020-08-24 22:57:29 +0530

    added backslash

---
+++ mutators/insertbklash.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#-:-:-:-:-:-:-::-:-:#
+#     SIP Torch     #
+#-:-:-:-:-:-:-::-:-:#
+
+# Author: 0xInfection
+# This module requires SIPTorch
+# https://github.com/0xInfection/SIPTorch
+
+import logging, random
+from libs.chars import BACKSLASH_SEQ1, BACKSLASH_SEQ2
+
+def insertBkSlash(msg: str):
+    '''
+    Insert backslash before the ending double quote
+    '''
+    log = logging.getLogger('insertBkSlash')
+    if not msg:
+        log.error("Nothing to apply transformation to")
+        return
+    if "\"" in msg:
+        # Replacing at the second occurance
+        seq = random.choice([BACKSLASH_SEQ1, BACKSLASH_SEQ2])
+        s = msg.replace('"', seq, 2).replace(seq, '"', 1)
+    return s
+
+print(insertBkSlash('aaaaaaaaaaaaaaaaaaaaaaa"aaaa"aaaaa'))
