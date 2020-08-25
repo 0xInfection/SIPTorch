5eae02d1a3faa8a455b95cca9622d3603ee93874
0xInfection <theinfecteddrake@gmail.com>
2020-08-25 14:51:42 +0530

    added multictl

---
+++ tests/application/multicl.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#-:-:-:-:-:-:-::-:-:#
+#     SIP Torch     #
+#-:-:-:-:-:-:-::-:-:#
+
+# Author: 0xInfection
+# This module requires SIPTorch
+# https://github.com/0xInfection/SIPTorch
+
+import logging, random
+from core.requester import buildreq
+from core.utils import parseMsg, catMetHead
+
+module_info = {
+    'category'  :   'Application Layer Semantics',
+    'test'      :   'OPTIONS with Multiple Content-Length Values',
+    'id'        :   'multicl'
+}
+
+def multicl():
+    '''
+    OPTIONS with Multiple Content-Length Values
+
+    If this request appeared over UDP, so the remainder of the datagram
+    can simply be discarded.  If a request like this arrives over TCP,
+    the framing error is not recoverable, and the connection should be
+    closed.
+    '''
+    log = logging.getLogger('multicl')
+    log.info('Testing module: %s' % module_info['test'])
+    msg = buildreq.makeRequest('OPTIONS')
+    mline, head, body = parseMsg(msg)
+    # Tweak 1: Add multiple content-length values
+    head['content-length'] = random.getrandbits(6)
+    # Recompiling our message
+    mg = catMetHead(mline, head, body=body)
+    return mg
