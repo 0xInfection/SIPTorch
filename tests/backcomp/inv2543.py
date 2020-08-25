7a14085ea8a2bde54f2972ceb41acd449761b823
0xInfection <theinfecteddrake@gmail.com>
2020-08-25 22:52:25 +0530

    Added badbranch

--- tests/backcomp/inv2543.py
+++ tests/backcomp/inv2543.py
@@ -10,18 +10,25 @@
 # https://github.com/0xInfection/SIPTorch
 
 import logging
+from core.plugrun import runPlugin
 from core.requester import buildreq
 from core.utils import parseMsg, catMetHead
 from mutators.replparam import rmallParam, rmspcParam
 
 module_info = {
     'category'  :   'Backward Compatability Tests',
-    'type'      :   '',
     'test'      :   'INVITE With RFC 2543 Syntax Support',
     'id'        :   'inv2543'
 }
 
 def inv2543():
+    '''
+    INVITE With RFC 2543 Syntax Support
+
+    This is a legal message per RFC 2543 (and several bis versions) that
+    should be accepted by RFC 3261 elements that want to maintain
+    backwards compatibility.
+    '''
     log = logging.getLogger('inv2543')
     log.info('Testing module: %s' % module_info['test'])
     msg = buildreq.makeRequest('INVITE')
@@ -29,11 +36,24 @@ def inv2543():
     # Tweak 1: Remove branch parameter from Via
     head['Via'] = rmallParam(head['Via'])
     # Tweak 2: Remove from tag
-    head['From'] = rmspcParam(head['From'], param='tag').replace('>', '') + ';user=phone>'
+    head['From'] = rmspcParam(head['From'], 
+        param='tag').replace('>', '') + ';user=phone>'
     # Tweak 3: Remove the following headers
-    rmhead = ('Allow', 'Contact', 'Max-Forwards', 'Content-Length')
+    rmhead = (  'Allow', 
+                'Contact', 
+                'Max-Forwards', 
+                'Content-Length'
+        )
     for x in rmhead:
         head.pop(x, None)
     # Forming the request message back up
     mg = catMetHead(mline, head, body=body)
     return mg
+
+def run(sock):
+    '''
+    Run this module by sending the actual request
+    '''
+    log = logging.getLogger('run')
+    if runPlugin(sock, inv2543()):
+        log.info('Module %s completed' % module_info['test'])
---
+++ tests/transaction/badbranch.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#-:-:-:-:-:-:-::-:-:#
+#     SIP Torch     #
+#-:-:-:-:-:-:-::-:-:#
+
+# Author: 0xInfection
+# This module requires SIPTorch
+# https://github.com/0xInfection/SIPTorch
+
+import logging, re
+from core.plugrun import runPlugin
+from core.requester import buildreq
+from core.utils import parseMsg, catMetHead
+from mutators.replparam import rmallParam, rmspcParam
+
+module_info = {
+    'category'  :   'Transaction Layer Semantics',
+    'test'      :   'Branch Tag Missing Transaction Identifier',
+    'id'        :   'badbranch'
+}
+
+def badbranch():
+    '''
+    Branch Tag Missing Transaction Identifier
+
+    This request indicates support for RFC 3261-style transaction
+    identifiers by providing the z9hG4bK prefix to the branch parameter,
+    but it provides no identifier.  A parser must not break when
+    receiving this message.  An element receiving this request could
+    reject the request with a 400 Response (preferably statelessly, as
+    other requests from the source are likely also to have a malformed
+    branch parameter), or it could fall back to the RFC 2543-style
+    transaction identifier.
+    '''
+    log = logging.getLogger('inv2543')
+    log.info('Testing module: %s' % module_info['test'])
+    msg = buildreq.makeRequest('INVITE')
+    mline, head, body = parseMsg(msg)
+    # Tweak 1: Remove the branch identifier after z9hG4bk
+    repl = re.sub(r'z9hG4bK.*?[^;]*', r'z9hG4bK', head['Via'])
+    head['Via'] = repl
+    # Forming the request message back up
+    msg = catMetHead(mline, head, body=body)
+    return msg
+
+def run(sock):
+    '''
+    Run this module by sending the actual request
+    '''
+    log = logging.getLogger('run')
+    if runPlugin(sock, badbranch()):
+        log.info('Module %s completed' % module_info['test'])
