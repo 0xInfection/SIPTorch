2218d052e000245c0e626bc322df5271dbe0c68f
0xInfection <theinfecteddrake@gmail.com>
2020-08-25 14:50:56 +0530

    added cparam2

---
+++ tests/application/cparam2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#-:-:-:-:-:-:-::-:-:#
+#     SIP Torch     #
+#-:-:-:-:-:-:-::-:-:#
+
+# Author: 0xInfection
+# This module requires SIPTorch
+# https://github.com/0xInfection/SIPTorch
+
+import logging
+from core.requester import buildreq
+from core.utils import parseMsg, catMetHead
+
+module_info = {
+    'category'  :   'Application Layer Semantics',
+    'test'      :   'REGISTER with a URL in Contact Header Parameter',
+    'id'        :   'cparam2'
+}
+
+def cparam2():
+    '''
+    REGISTER with a URL in Contact Header Parameter
+
+    This register request contains a contact where the URI has an unknown
+    parameter.
+
+    The register should succeed, and a subsequent retrieval of the
+    registration must include "unknownparam" as a url-parameter.
+    '''
+    log = logging.getLogger('cparam2')
+    log.info('Testing module: %s' % module_info['test'])
+    msg = buildreq.makeRequest('REGISTER')
+    mline, head, body = parseMsg(msg)
+    # Tweak 1: Add an unknwown param to contact header
+    head['Contact'] += ';unknownparam'
+    # Tweak 2: Add the </> to make a complete URL
+    head['Contact'] = '<%s>' % head['Contact']
+    # Forming the request message back up
+    mg = catMetHead(mline, head, body=body)
+    return mg
