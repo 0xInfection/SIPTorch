40958e47092e4b64ffe2d7e418822f6cbc407841
0xInfection <theinfecteddrake@gmail.com>
2020-09-06 23:04:33 +0530

    working main code

--- core/main.py
+++ core/main.py
@@ -9,13 +9,15 @@
 # This module requires SIPTorch
 # https://github.com/0xInfection/SIPTorch
 
+import sys
 import time
 import socket
 import logging
-from core import config
 from core.options import *
-from core.plugrun import runAll
+from libs import config
 from core.colors import G
+from core.plugrun import runAll
+#from core.options import setsipopts
 from core.utils import calcLogLevel
 from core.requester import connector
 from core.logger import loggerinit, logfooter, CustomFormatter
@@ -24,14 +26,14 @@ def startEngine():
     '''
     The main stuff
     '''
+    #args, config = setsipopts()
     formatter = CustomFormatter()
     handler = logging.StreamHandler(sys.stdout)
     handler.setFormatter(fmt=formatter)
     logging.root.addHandler(hdlr=handler)
     logging.root.setLevel(level=calcLogLevel(args))
     timestart = time.time()
-    loggerinit()
-    runAll()
+    runAll(loggerinit(host=config.RHOST))
     print(G, 'All modules completed.')
     timeend = time.time()
     print(G, 'Total time taken: %.3fs' % (timeend - timestart))
